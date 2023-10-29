a7c55c37db0800cdd2c9cd0bfc22242c2f8f7696
Zachary Talmadge <zach.talmadge.webdev@gmail.com>
2023-10-29 17:13:31 -0400

    pass tests

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,22 @@
 class MySet:
 
-    pass
+    def __init__(self, enumerable = []):
+        self.dictionary = {}
+        for value in enumerable:
+            self.dictionary[value] = True
+    
+    def has(self, value):
+        return value in self.dictionary
+    
+    def add(self, value):
+        self.dictionary[value] = True # Add a value as a key on the Dictionary
+        return self                   # Return the updated set
+    
+    def delete(self, value):
+        self.dictionary.pop(value, None)
+        return self
+    
+    def size(self):
+        return len(self.dictionary)
+
+
